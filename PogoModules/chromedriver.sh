2edc7d8466255d144a8be275bb723f6782403c9c
LoranceTseng <mdx121843@gmail.com>
2022-01-29 20:32:21 +0800

    .

---
+++ PogoModules/chromedriver.sh
@@ -0,0 +1,10 @@
+# Versions
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+rm ~/chromedriver_linux64.zip
+sudo rm /usr/local/bin/chromedriver
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
--- throughput.py
+++ throughput.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     width = ['24w20','24w2040','24w40','5w20','5w40','5w80']
 
     for i in width:
-        Config_File_Upload2('Config/'+ i +'.txt')
+        #Config_File_Upload2('Config/'+ i +'.txt')
+        print('Config/'+ i +'.txt')
         Bsleep(120)
         #print('download' + str(i))
         #print('upload' + str(i))
